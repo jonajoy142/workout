1c025c63a3cad3b9c711696b04ea27453dfa221a
Jona Joy <jonajoy142@gmail.com>
2022-12-29 15:40:50 +0530

    JWT Created

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,11 @@
 // login user
 const User = require('../models/userModel')
-const validator = require('validator')
+// const validator = require('validator')
+const jwt  = require('jsonwebtoken')
+
+const createToken = (_id)=>{
+  return jwt.sign({_id:_id},process.env.SECRET,{expiresIn:'3d'})
+}
 
 const loginUser = async(req,res)=>{
     res.json({mssg:'login user'})
@@ -11,7 +16,11 @@ const signupUser = async(req,res)=>{
     const {email,password} = req.body
     try{
      const user = await User.signup(email,password)
-     res.status(200).json({email,user})
+
+     //create Tokeken
+
+     const token = createToken(user._id)
+     res.status(200).json({email,token})
     }catch(error){
       res.status(400).json({error:error.message})
     }
